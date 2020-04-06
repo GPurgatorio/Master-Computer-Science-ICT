0ee5b39893fb6b92c92788c022c7fc3a841634f3
Jacopo <jacopomassa97@gmail.com>
2020-04-06 16:55:17 +0200

    First commit for Bluetooth authentication factor

---
+++ auth_factors/mcps-bluetooth.py
@@ -0,0 +1,31 @@
+import bluetooth
+
+
+def __dictify_device(device):
+    keys = ('address', 'name')
+
+    # decode the address (convert from bytes to str)
+    address = device[0].decode(encoding='utf-8', errors='strict')
+    name = device[1]
+    values = [address, name]
+    return dict(zip(keys, values))
+
+
+def discover_devices():
+    print("Performing inquiry...")
+
+    nearbly_devices = bluetooth.discover_devices(duration=5, lookup_names=True, flush_cache=True)
+
+    print("I've found {} devices".format(len(nearbly_devices)))
+
+    formatted_devices = [__dictify_device(d) for d in nearbly_devices]
+    return formatted_devices
+
+
+def get_local_device_address():
+    return bluetooth.read_local_bdaddr()[0].decode(encoding='utf-8', errors='strict')
+
+
+if __name__ == '__main__':
+    print("LOCAL ADDRESS:", get_local_device_address())
+    print(discover_devices())
