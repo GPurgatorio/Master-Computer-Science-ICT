406410e40ea5acf9698bbb00ef6c1b357c24e39b
Giovanni <giovanni.bartolomeo.rg@gmail.com>
2020-04-22 16:38:07 +0200

    added door-lock service

---
+++ door_service/config.py
@@ -0,0 +1,11 @@
+MQTT_SERVER = "raspberrypigio2.zapto.org"
+MQTT_ID = "giobart"
+MQTT_PASS = "qwerty99"
+MQTT_SERVER_TEST = "raspberrypigio2.zapto.org"
+MQTT_ID_TEST = "giobart"
+MQTT_PASS_TEST = "qwerty99"
+DEVICE_ID = "door_lock_1"
+DOOR_MESSAGE_RESPONSE = "door/open"
+OPEN_DOOR_PIN = 21
+ERROR_PIN = 20
+TEST_MACHINE = True
