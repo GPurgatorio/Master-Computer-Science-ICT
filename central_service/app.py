b311d7f17eefe1671521b8ba62522a784195b55c
Antonio Boffa <a.boffa95@gmail.com>
2020-04-26 19:42:26 +0200

    database populated with API calls

--- central_service/app.py
+++ central_service/app.py
@@ -1,22 +1,29 @@
 #!flask/bin/python
 from flask import Flask, request, jsonify, session, render_template, redirect, url_for, flash
 from flask_pymongo import PyMongo
-from bson.json_util import loads, dumps
 import bcrypt
 
+''' If we want to be secure
 secrets = open("super_secret.txt", "r")
 secrets_splitted = secrets.read().split()
 mongodb_pass = secrets_splitted[0]
 session_secret = secrets_splitted[1]
+'''
+#else
+mongodb_pass = "chessarmi"
+session_secret = "mysecret"
 
 app = Flask(__name__)
 
-app.config["MONGO_URI"] = "mongodb+srv://chessarmi:"+mongodb_pass+"@mcps-project-txguh.mongodb.net/chessaRMI?retryWrites=true&w=majority"
+app.config["MONGO_URI"] = "mongodb+srv://chessarmi:"+mongodb_pass + \
+	"@mcps-project-txguh.mongodb.net/chessaRMI?retryWrites=true&w=majority"
 
 mongo = PyMongo(app)
 
+
 def is_logged(session):
-	return  'name' in session and 'surname' in session
+	return 'name' in session and 'surname' in session
+
 
 @app.route("/")
 @app.route("/home")
@@ -59,6 +66,7 @@ def logout():
 # MOCK endpoints. Administrators stuff. Only with PostMan
 #
 
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     # Use Postman!
@@ -74,7 +82,8 @@ def register():
                           'password': hashpass,
                           'name': json_request['name'],
                           'surname': json_request['surname'],
-                          'hash_faces': 0
+                          'face_code': json_request['face_code'],
+                          "rooms" : []
                           })
             return "OK!", 201
 
@@ -89,7 +98,9 @@ def register():
 def users():
     users_to_return = []
     for user in mongo.db.users.find():
-        users_to_return.append(dumps(user))
+        user['_id'] = str(user['_id'])
+        user['password'] = str(user['password'])
+        users_to_return.append(user)
     return jsonify(users_to_return), 200
 
 
@@ -103,7 +114,8 @@ def users_id(id):
 def rooms():
     rooms_to_return = []
     for room in mongo.db.rooms.find():
-        rooms_to_return.append(dumps(room))
+        room['_id'] = str(room['_id'])
+        rooms_to_return.append(room)
     return jsonify(rooms_to_return), 200
 
 
@@ -112,12 +124,18 @@ def room_id(id):
     room_to_return = mongo.db.rooms.find_one_or_404({"_id": id})
     return jsonify(room_to_return), 200
 
+@app.route('/api/v0.0/rooms/new', methods=['POST'])
+def room_new():
+    json_request = request.get_json()
+    mongo.db.rooms.insert({'name': json_request['name'], "users_allowed" : []})
+    return "OK!", 201
 
 @app.route('/api/v0.0/policies', methods=['GET', 'POST', 'PUT'])
 def policies():
     policies_to_return = []
     for policy in mongo.db.policies.find():
-        policies_to_return.append(dumps(policy))
+        policy['_id'] = str(policy['_id'])
+        policies_to_return.append(policy)
     return jsonify(policies_to_return), 200
 
 
@@ -129,23 +147,21 @@ def policy_id(id):
 
 @app.route('/api/v0.0/policies/new', methods=['GET', 'POST'])
 def boffa():
-    if request.method == 'GET':
-        # render template
-        return "To do", 200
-
+    json_request = request.get_json()
+    user_for_policie = mongo.db.users.find_one_or_404({"email": json_request["email"]})
+    room_for_policie = mongo.db.rooms.find_one_or_404({"name": json_request["room_name"]})
     if request.method == 'POST':
-        # user must be logged in
-        if is_logged(session):
-            policies = mongo.db.policies
-            policies.insert(
-                {
-                    "user_id": request.form['user_id'],
-                    "room_id": request.form['room_id'],
-                }
-            )
-        else:
-            return render_template("login.html")
+        policies = mongo.db.policies
+        policies.insert(
+            {
+                "user_id": user_for_policie["_id"],
+                "room_id": room_for_policie["_id"]
+            }
+        )
+        mongo.db.users.update_one({"_id" : user_for_policie["_id"]}, {'$push': {'rooms': room_for_policie["_id"]}})
+        mongo.db.rooms.update_one({"_id" : room_for_policie["_id"]}, {'$push': {'users_allowed': user_for_policie["_id"]}})
 
+    return "OK!", 201
 
 if __name__ == '__main__':
     app.secret_key = session_secret
