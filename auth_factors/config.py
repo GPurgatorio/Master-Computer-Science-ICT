acc1b47d7e3f2743d98aa8b77e6cac10cba2ba19
Jacopo <jacopomassa97@gmail.com>
2020-04-27 17:37:04 +0200

    Added expiration date. Started testing connection with MQTT broker

---
+++ auth_factors/config.py
@@ -0,0 +1,11 @@
+MQTT_SERVER = "raspberrypigio2.zapto.org"
+MQTT_PORT = 8883
+MQTT_ID = "giobart"
+MQTT_PASS = "qwerty99"
+MQTT_WAIT_TIME = 5
+
+BT_DISCOVER_TOPIC = "bluetooth/discover"
+BT_TIME_DELTA = 5
+EXPIRATION_DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
+
+DOOR_ID = "door_bt_1"
